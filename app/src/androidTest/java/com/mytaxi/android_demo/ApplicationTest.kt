0c6e3b4665f9715fdabc19157090de8fe7b6eb0a
torolbaeva <toktobubu.orolbaeva@zensoft.io>
2019-03-23 17:48:22 +0600

    Added tests for check Login

--- app/src/androidTest/java/com/mytaxi/android_demo/ApplicationTest.kt
+++ app/src/androidTest/java/com/mytaxi/android_demo/ApplicationTest.kt
@@ -1,6 +1,7 @@
 package com.mytaxi.android_demo
 
 
+import android.os.SystemClock
 import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.Espresso.onData
 import android.support.test.espresso.Espresso.onView
@@ -10,8 +11,8 @@ import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.mytaxi.android_demo.activities.MainActivity
-import org.hamcrest.CoreMatchers.anything
-import org.hamcrest.CoreMatchers.equalTo
+import com.mytaxi.android_demo.fixtures.LoginVariables
+import org.hamcrest.Matchers.*
 import org.junit.FixMethodOrder
 import org.junit.Rule
 import org.junit.Test
@@ -30,24 +31,24 @@ class ApplicationTest {
     fun test_00_userAppContext() {
         val appContext = InstrumentationRegistry.getTargetContext()
         equalTo(listOf("mytaxi.android_demo", appContext.packageName))
-        onView(withId(R.id.edt_username)).perform(typeText("crazydog335"))
-        onView(withId(R.id.edt_password)).perform(typeText("venture"))
-        onView(withId(R.id.btn_login)).perform(click())
-        onView(withId(R.id.textSearch)).perform(typeText("sa"))
-        onData(anything())
-                .atPosition(1)
-                .perform(click())
     }
 
-//    @Test
-//    fun test_01_user_can_enter_username() {
-//    onView(withId(R.id.edt_username)).perform(typeText("crazydog335"))
-//    }
-//
-//    @Test
-//    fun test_02_user_can_enter_password() {
-//        onView(withId(R.id.edt_password)).perform(typeText("venture"))
-//    }
+    @Test
+    fun test_01_Authorization() {
+        onView(withId(R.id.edt_username)).perform(typeText(LoginVariables.username))
+        onView(withId(R.id.edt_password)).perform(typeText(LoginVariables.password))
+        onView(withId(R.id.btn_login)).perform(click())
+
+    }
+
+    @Test
+    fun test_02_SearchByNameAndSelectSecondNameFromDropDown() {
+        SystemClock.sleep(600)
+        onView(withId(R.id.textSearch)).perform(typeText("sa"))
+        SystemClock.sleep(6000)
+        onData(anything()).inAdapterView(withId(R.id.place_autocomplete_powered_by_google)).atPosition(1).perform(click())
+        SystemClock.sleep(6000)
+    }
 //
 //    @Test
 //    fun test_03_user_can_click_login_button() {
---
+++ app/src/androidTest/java/com/mytaxi/android_demo/fixtures/LoginVariables.kt
@@ -0,0 +1,6 @@
+package com.mytaxi.android_demo.fixtures
+
+object LoginVariables {
+    val username = "crazydog335"
+    val password = "venture"
+}
