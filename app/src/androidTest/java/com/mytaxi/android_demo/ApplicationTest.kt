499ae3a8b32c512226d0a5b3e2e85a68a5c58233
torolbaeva <toktobubu.orolbaeva@zensoft.io>
2019-03-25 19:55:40 +0600

    Added tests for check Login

--- app/src/androidTest/java/com/mytaxi/android_demo/ApplicationTest.kt
+++ app/src/androidTest/java/com/mytaxi/android_demo/ApplicationTest.kt
@@ -3,21 +3,27 @@ package com.mytaxi.android_demo
 
 import android.os.SystemClock
 import android.support.test.InstrumentationRegistry
-import android.support.test.espresso.Espresso.onData
 import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.action.ViewActions.click
-import android.support.test.espresso.action.ViewActions.typeText
-import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.RootMatchers
+import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.mytaxi.android_demo.activities.MainActivity
 import com.mytaxi.android_demo.fixtures.LoginVariables
-import org.hamcrest.Matchers.*
+import org.hamcrest.Matchers.equalTo
 import org.junit.FixMethodOrder
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.MethodSorters
+import android.support.test.espresso.matcher.ViewMatchers.withParent
+import android.support.v7.widget.AppCompatImageButton
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.*
+import android.support.v7.widget.Toolbar
+import org.hamcrest.Matchers.allOf
+
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(AndroidJUnit4::class)
@@ -35,28 +41,49 @@ class ApplicationTest {
 
     @Test
     fun test_01_Authorization() {
-        onView(withId(R.id.edt_username)).perform(typeText(LoginVariables.username))
-        onView(withId(R.id.edt_password)).perform(typeText(LoginVariables.password))
-        onView(withId(R.id.btn_login)).perform(click())
-
+        onView(withId(R.id.edt_username))
+                 .check(matches(isDisplayed()))
+                 .perform(clearText())
+                 .perform(typeText(LoginVariables.username))
+        onView(withId(R.id.edt_password))
+                .check(matches(isDisplayed()))
+                .perform(clearText())
+                .perform(typeText(LoginVariables.password))
+        onView(withId(R.id.btn_login))
+                .check(matches(isDisplayed()))
+                .perform(click())
     }
 
     @Test
-    fun test_02_SearchByNameAndSelectSecondNameFromDropDown() {
+    fun test_02_SearchByNameAndSelectSecondNameFromDropDownThenClickCallButton() {
         SystemClock.sleep(600)
-        onView(withId(R.id.textSearch)).perform(typeText("sa"))
-        SystemClock.sleep(6000)
-        onData(anything()).inAdapterView(withId(R.id.place_autocomplete_powered_by_google)).atPosition(1).perform(click())
-        SystemClock.sleep(6000)
+        onView(withId(R.id.textSearch))
+                .perform(clearText())
+                .perform(typeText("sa"))
+                .check(matches(isDisplayed()))
+        SystemClock.sleep(600)
+        onView(withText("Sarah Scott"))
+                .inRoot(RootMatchers.isPlatformPopup())
+                .perform(click())
+        SystemClock.sleep(600)
+        onView(withId(R.id.imageViewDriverAvatar))
+                .check(matches(isDisplayed()))
+        SystemClock.sleep(600)
+        onView(withId(R.id.textViewDriverName))
+                .check(matches(withText("Sarah Scott")))
+        SystemClock.sleep(600)
+        onView(withId(R.id.imageViewDriverLocation))
+                .check(matches(isDisplayed()))
+        onView(withId(R.id.textViewDriverLocation))
+                .check(matches(withText("6834 charles st")))
+        SystemClock.sleep(600)
+        onView(withId(R.id.imageViewDriverDate))
+        SystemClock.sleep(600)
+        onView(withId(R.id.textViewDriverDate))
+                .check(matches(withText("2002-10-18")))
+        SystemClock.sleep(600)
+        onView(withId(R.id.fab))
+                .check(matches(isDisplayed()))
+                .perform(click())
     }
-//
-//    @Test
-//    fun test_03_user_can_click_login_button() {
-//        onView(withId(R.id.btn_login)).perform(click())
-//    }
-//
-//    @Test
-//    fun test_04_user_can_enter_text_for_search() {
-//        onView(withId(R.id.search_bar)).perform(typeText("sa"))
-//    }
 }
