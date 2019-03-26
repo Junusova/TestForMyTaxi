008a1284a273b388f9f6fd2f4332d7df50533162
torolbaeva <toktobubu.orolbaeva@zensoft.io>
2019-03-26 12:16:27 +0600

    Added Task

---
+++ app/src/androidTest/java/AcceptanceTestSuite.kt
@@ -0,0 +1,10 @@
+import com.mytaxi.android_demo.ApplicationTest
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+        ApplicationTest::class
+)
+
+class AcceptanceTestSuite
